977f181abfb7e5898bfd9ac8c9fc591f8c919b57
Ryan DeBeasi <hi@ryandebeasi.com>
2017-03-29 17:15:13 -0400

    Add existing code from multimedia repo and generate package.json.

---
+++ index.js
@@ -0,0 +1,11 @@
+var csvLoader = require( 'lib/csv-loader' );
+var setDefaults = require( 'lib/defaults' );
+var colors = require( 'lib/colors' );
+var text = require( 'lib/text' );
+
+module.exports = {
+  csvLoader: csvLoader,
+  setDefaults: setDefaults,
+  colors: colors,
+  text: text
+};
